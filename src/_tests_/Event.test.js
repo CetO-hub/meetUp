55ee40ee970fcdcdffe388fdc6e53786faadd1da
CetO-hub <skynet@SkyNet.localdomain>
2023-01-27 15:35:26 +0100

    feat: Complete unit testing for 2nd feature

--- src/_tests_/Event.test.js
+++ src/_tests_/Event.test.js
@@ -24,4 +24,72 @@ describe("<Event/> component", () => {
       `@${event.summary} | ${event.location}`
     );
   });
+  test("render the show details button", () => {
+    expect(EventWrapper.find(".show-details")).toHaveLength(1);
+  });
+  test("render the show details button", () => {
+    expect(EventWrapper.find(".show-details").text()).toBe("Show Details");
+  });
+  test("change `visible` state when `Show Details` button is clicked", () => {
+    EventWrapper.setState({
+      visible: false,
+    });
+    const eventObject = { target: { value: true } };
+    EventWrapper.find(".show-details").simulate("click", eventObject);
+    expect(EventWrapper.state("visible")).toBe(true);
+  });
+  test("change button text from `Show Details` to `Hide Details` when button is clicked", () => {
+    EventWrapper.setState({
+      visible: false,
+    });
+    const eventObject = { target: { value: true } };
+    EventWrapper.find(".show-details").simulate("click", eventObject);
+    expect(EventWrapper.find(".show-details").text()).toBe("Hide Details");
+  });
+  test("show details section when `Show Details` button is clicked", () => {
+    EventWrapper.setState({
+      visible: false,
+    });
+    const eventObject = { target: { value: true } };
+    EventWrapper.find(".show-details").simulate("click", eventObject);
+    expect(EventWrapper.find(".details")).toHaveLength(1);
+  });
+  test("show heading of the details section", () => {
+    EventWrapper.setState({
+      visible: false,
+    });
+    const eventObject = { target: { value: true } };
+    EventWrapper.find(".show-details").simulate("click", eventObject);
+    expect(EventWrapper.find(".details-heading").text()).toBe("About Event:");
+  });
+  test("show html link to the calender in the details section", () => {
+    EventWrapper.setState({
+      visible: false,
+    });
+    const eventObject = { target: { value: true } };
+    EventWrapper.find(".show-details").simulate("click", eventObject);
+    expect(EventWrapper.find(".details-link").text()).toBe(
+      "See details on Google Calender"
+    );
+  });
+  test("link to the calender is clickable in the details section", () => {
+    EventWrapper.setState({
+      visible: false,
+    });
+    const eventObject = { target: { value: true } };
+    EventWrapper.find(".show-details").simulate("click", eventObject);
+    expect(EventWrapper.find(".details-link").prop("href")).toBe(
+      event.htmlLink
+    );
+  });
+  test("show event description in the details section", () => {
+    EventWrapper.setState({
+      visible: false,
+    });
+    const eventObject = { target: { value: true } };
+    EventWrapper.find(".show-details").simulate("click", eventObject);
+    expect(EventWrapper.find(".details-description").text()).toBe(
+      event.description
+    );
+  });
 });
--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+  state = {
+    visible: false,
+  };
+
+  handleClick = () => {
+    this.setState({ visible: !this.state.visible });
+  };
+
   render() {
     const { event } = this.props;
     const date = new Date(event.start.dateTime);
@@ -10,6 +18,18 @@ class Event extends Component {
           <h1 className="event-summary">{event.summary}</h1>
           <p className="event-time">{`${date}`}</p>
           <p className="event-location">{`@${event.summary} | ${event.location}`}</p>
+          {this.state.visible && (
+            <div className="details">
+              <h2 className="details-heading">About Event:</h2>
+              <a href={event.htmlLink} className="details-link">
+                See details on Google Calender
+              </a>
+              <p className="details-description">{event.description}</p>
+            </div>
+          )}
+          <button className="show-details" onClick={this.handleClick}>
+            {!this.state.visible ? "Show Details" : "Hide Details"}
+          </button>
         </div>
       </>
     );
