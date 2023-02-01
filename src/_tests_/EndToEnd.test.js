3fb2f7e2fe0560c8667309701a5ce24723a17e62
CetO-hub <skynet@SkyNet.localdomain>
2023-02-01 14:26:57 +0100

    feat: Add acceptance and end to end testing for first feature

---
+++ src/_tests_/EndToEnd.test.js
@@ -0,0 +1,34 @@
+import puppeteer from "puppeteer";
+
+describe("show/hide event details", () => {
+  let page, browser;
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 250,
+      ignoreDefaultArgs: ["--disable-extensions"],
+      args: ["--no-sandbox"],
+    });
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+    await page.waitForSelector(".event-overview");
+  });
+  afterAll(() => {
+    browser.close();
+  });
+  test("An event element is collapsed by default", async () => {
+    const eventDetails = await page.$(".event-overview .details");
+    expect(eventDetails).toBe(null);
+  });
+  test("user can expend an event to see its details", async () => {
+    await page.click(".show-details");
+    const eventDetails = await page.$(".event-overview .details");
+    expect(eventDetails).toBeDefined();
+  });
+  test("user can collapse the event to hide its details", async () => {
+    await page.click(".show-details");
+    const eventDetails = await page.$(".event-overview .details");
+    expect(eventDetails).toBe(null);
+  });
+});
