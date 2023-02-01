ff4f6f5c4dd31c8d0b58d70f64c70512465b6984
CetO-hub <skynet@SkyNet.localdomain>
2023-02-01 16:15:07 +0100

    fix: remove atatus test error

--- src/index.js
+++ src/index.js
@@ -6,5 +6,3 @@ import * as atatus from "atatus-spa";
 atatus.config("9cfab6d35c2a42f888ebb75148b95963").install();
 const root = document.getElementById("root");
 render(<App />, root);
-
-atatus.notify(new Error("Test Atatus Setup"));
