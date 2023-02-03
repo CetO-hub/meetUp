79ccea340a4865f649cf94394171e9fb4ccc62e6
CetO-hub <skynet@SkyNet.localdomain>
2023-02-03 13:29:16 +0100

    feat: Add PWA

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 import * as atatus from "atatus-spa";
 atatus.config("9cfab6d35c2a42f888ebb75148b95963").install();
 const root = document.getElementById("root");
 render(<App />, root);
+
+serviceWorkerRegistration.register();
